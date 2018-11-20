c8d958e3900d128da76f69ce184dd03c45342800
Yashrif Hasib <33043549+yhasib6487@users.noreply.github.com>
2018-11-20 14:15:14 -0500

    Add .gitignore file.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main( String[] args ) {
         int[] scrambled = { 654, 6541, 978, 614, 415, 123, 48};
+        String[] scramble = { "Yashrif", "Nukem", "Dukem", "Bookem" };
         Sorts.Unscrambler( scrambled );
+        Sorts.Unscrambler( scramble );
         System.out.println( Arrays.toString( scrambled ) );
+        System.out.println( Arrays.toString( scramble ) );
     }
 }
--- src/Sorts.java
+++ src/Sorts.java
@@ -11,8 +11,26 @@ public class Sorts {
         }
     }
 
+    public static void Unscrambler( String[] scrambled ) {
+        for (int i = 0; i < scrambled.length - 1; i++) {
+            int lowPosition = i;
+            for (int j = i + 1; j < scrambled.length; j++) {
+                if ( scrambled[ j ].compareToIgnoreCase( scrambled[ lowPosition ] ) < 0 ) {
+                    lowPosition = j;
+                }
+            }
+            swap(i, lowPosition, scrambled);
+        }
+    }
+
     private static void swap( int indexOne, int indexTwo, int[] arr ) {
         int temp = arr[ indexOne ];
+        arr[ indexOne ] = arr[ indexTwo ];
+        arr[ indexTwo ] = temp;
+    }
+
+    private static void swap( int indexOne, int indexTwo, String[] arr ) {
+        String temp = arr[ indexOne ];
         arr[ indexOne ] = arr[ indexTwo ];
         arr[ indexTwo ] = temp;
     }
