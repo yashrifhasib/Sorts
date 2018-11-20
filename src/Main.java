4b34c7084333e81b4b4b4b0e08d77b83e3aad7f6
Yashrif Hasib <33043549+yhasib6487@users.noreply.github.com>
2018-11-20 14:17:07 -0500

    Add .gitignore file.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main( String[] args ) {
         int[] scrambled = { 654, 6541, 978, 614, 415, 123, 48};
-        String[] scramble = { "Yashrif", "Nukem", "Dukem", "Bookem" };
+        String[] scrambledString = { "Yashrif", "Nukem", "Dukem", "Bookem" };
         Sorts.Unscrambler( scrambled );
-        Sorts.Unscrambler( scramble );
+        Sorts.Unscrambler( scrambledString );
         System.out.println( Arrays.toString( scrambled ) );
-        System.out.println( Arrays.toString( scramble ) );
+        System.out.println( Arrays.toString( scrambledString ) );
     }
 }
